ece417b7a4a55044a0f58e8e356a3bbeb75221da
ABDULRAHMAN SARMINI <134057752+Abodnk5@users.noreply.github.com>
2025-03-19 13:18:20 +0300

    Create Biggest Single Number.sql

---
+++ leetcode_50/Sorting and Grouping/Biggest Single Number.sql
@@ -0,0 +1,26 @@
+/*
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| num         | int  |
++-------------+------+
+This table may contain duplicates (In other words, there is no primary key for this table in SQL).
+Each row of this table contains an integer.
+ 
+
+A single number is a number that appeared only once in the MyNumbers table.
+
+Find the largest single number. If there is no single number, report null.
+
+The result format is in the following example.
+
+ 
+*/
+ABDULRAHMAN KASHO SARMINI | 240757904
+select max(num) as num from
+(select num,count(num) as freq
+from MyNumbers
+group by num
+) as counted
+where freq = 1
