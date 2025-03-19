521369225c089443ad1f329e5e7ececdfd648499
ABDULRAHMAN SARMINI <134057752+Abodnk5@users.noreply.github.com>
2025-03-19 13:08:16 +0300

    Create Queries Quality and Percentage.sql

---
+++ leetcode_50/Basic Aggregate Functions/Queries Quality and Percentage.sql
@@ -0,0 +1,42 @@
+/*
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| query_name  | varchar |
+| result      | varchar |
+| position    | int     |
+| rating      | int     |
++-------------+---------+
+This table may have duplicate rows.
+This table contains information collected from some queries on a database.
+The position column has a value from 1 to 500.
+The rating column has a value from 1 to 5. Query with rating less than 3 is a poor query.
+ 
+
+We define query quality as:
+
+The average of the ratio between query rating and its position.
+
+We also define poor query percentage as:
+
+The percentage of all queries with rating less than 3.
+
+Write a solution to find each query_name, the quality and poor_query_percentage.
+
+Both quality and poor_query_percentage should be rounded to 2 decimal places.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+*/
+ABDULRAHMAN KASHO SARMINI | 240757904
+
+select query_name,
+ROUND(AVG(rating/position), 2) as quality,
+ROUND(100.0*sum(if(rating<3, 1,0))/count(1),2) as poor_query_percentage
+from Queries
+group by query_name
+order by 2 desc;
